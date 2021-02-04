e35d2ef92fba7153015da851153861e9963f85c7
Edgar Hovsepyan <hovsepyan.edgar@softconstruct.local>
2021-02-04 19:20:01 +0400

    Create Task Tracker

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task, i) => (
+        <Task key={i} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
